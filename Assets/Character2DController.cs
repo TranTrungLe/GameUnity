24e40b7fa7abfc8bc643a446389f032d89cef15d
TranTrungLe <trantrungle07051999@gmail.com>
2020-11-25 14:47:33 +0700

    sumit

---
+++ Assets/Character2DController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D rgid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rgid.velocity.y) < 0.01)
+        {
+            rgid.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
